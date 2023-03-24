82203d6b72d58010cafde56a99e4c88e795594cf
rowanspake <109189967+rowanspake@users.noreply.github.com>
2023-03-23 22:43:02 -0700

    Created ConsoleAppAssn

---
+++ ConsoleAppAssn/ConsoleAppAssn/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleAppAssn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Gimme a number"); //Asks for input
+            string userInput = Console.ReadLine(); //Gets input data
+            int userInputInt = Convert.ToInt32(userInput); //Converts it to integer
+            int mathIt = userInputInt * 50; //Math's it
+            Console.WriteLine(mathIt); //Writes it
+
+            Console.WriteLine("Gimme another one");
+            userInput = Console.ReadLine();
+            userInputInt = Convert.ToInt32(userInput);
+            mathIt = userInputInt + 25;
+            Console.WriteLine(mathIt);
+
+            Console.WriteLine("Gimme another one");
+            userInput = Console.ReadLine();
+            double userInputDouble = Convert.ToDouble(userInput); //Floating point this time
+            double mathItDouble = userInputDouble / 12.5;
+            Console.WriteLine(mathItDouble);
+
+            Console.WriteLine("Gimme another one");
+            userInput = Console.ReadLine();
+            userInputInt = Convert.ToInt32(userInput);
+            bool userInputBool = userInputInt > 50; //Bool it
+            Console.WriteLine(userInputBool);
+
+            Console.WriteLine("Gimme another one");
+            userInput = Console.ReadLine();
+            userInputDouble = Convert.ToDouble(userInput);
+            mathItDouble = userInputDouble % 7; //Modulize it
+            Console.WriteLine(mathItDouble);
+            Console.WriteLine("All done");
+            Console.ReadLine(); 
+        }
+    }
+}
---
+++ MathChallenge/MathChallenge/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace MathChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            short num1 = 2;
+            short num2 = 5;
+            Console.WriteLine(num1 + num2); 
+            Console.WriteLine(num1 - num2);
+            Console.WriteLine(num1 * num2);
+            Console.WriteLine(num1 / num2);
+            float num3 = 2;
+            float num4 = 5;
+            Console.WriteLine(num3 / num4);
+            //Console.ReadLine();
+
+            int product = 10 * 5;
+            Console.WriteLine(product);
+            //Console.ReadLine();
+
+            double quotient = 100.0 / 17.0;
+            Console.WriteLine(quotient);
+            //Console.ReadLine();
+
+            int remainder = 11 % 2;
+            Console.WriteLine(remainder);
+            //Console.ReadLine();
+
+            bool trueOrFalse = 12 < 5;
+            Console.WriteLine(trueOrFalse);
+            //Console.ReadLine();
+
+            int roomTemp = 70;
+            int currentTemp = 72;
+            bool isWarm = currentTemp >= roomTemp;
+            Console.WriteLine(isWarm);
+            Console.ReadLine();
+        }
+    }
+}
