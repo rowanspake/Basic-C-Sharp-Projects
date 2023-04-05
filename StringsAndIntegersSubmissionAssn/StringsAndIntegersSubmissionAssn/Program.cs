05e4da5fc522f3320437dd8ad372775e2d44663b
rowanspake <109189967+rowanspake@users.noreply.github.com>
2023-04-05 12:19:09 -0700

    Commented StringsAndIntegersSubmissionAssn

--- StringsAndIntegersSubmissionAssn/StringsAndIntegersSubmissionAssn/Program.cs
+++ StringsAndIntegersSubmissionAssn/StringsAndIntegersSubmissionAssn/Program.cs
@@ -7,27 +7,37 @@ namespace StringsAndIntegersSubmissionAssn
     {
         static void Main(string[] args)
         {
-            List<int> integerList = new List<int>() { 10, 20, 30, 40, 50};
+            //List of integers
+            List<int> integerList = new List<int>() { 10, 20, 30, 40, 50}; 
 
             try
             {
                 Console.WriteLine("Let's do some division.\nGive me a number to divide by.");
+                //Get user input number and make it an integer
                 int userInput = Convert.ToInt32(Console.ReadLine());
+                //Iterate through list and divide contents by the input
                 for (int i = 0; i < integerList.Count; i++)
+                    //Output the results
                     Console.WriteLine(integerList[i] + " divided by " + userInput + " is " + integerList[i] / userInput);
             }
+            //Oops, not a number
             catch (FormatException ex)
             {
+                //Tell 'em
                 Console.WriteLine(ex.Message);
             }
+            //Can't divide by zero
             catch (DivideByZeroException ex)
             {
+                //Nuh-uh
                 Console.WriteLine(ex.Message);
             }
             finally
             {
+                //Peace out
                 Console.WriteLine("Alright, done with that.  Let's continue!");
             }
+            //We're done with all that.
             Console.WriteLine("Just kidding, that's all for now.  Good bye!");
         }
     }
