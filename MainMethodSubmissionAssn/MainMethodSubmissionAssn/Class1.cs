ee22f341bd2479b00ba38a791e83c5e2a37ca471
rowanspake <109189967+rowanspake@users.noreply.github.com>
2023-04-13 22:55:22 -0700

    Created MainMethodSubmissionAssn project

---
+++ MainMethodSubmissionAssn/MainMethodSubmissionAssn/Class1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MainMethodSubmissionAssn
+{
+    //Create our class
+    public class Class1
+    {
+        //Declare the fields for our class constructor
+        public int number;
+        public decimal decNumber;
+        public string word;
+
+        //Declare our class constructor and initial values for fields
+        public Class1()
+        {
+            number = 0;
+            decNumber = 0;
+            word = "";
+        }
+
+        //First method, taking in and putting out an integer
+        public static int Method1(int number1)
+        {
+            //Math
+            int output = number1 + 100;
+            return output;
+        }
+
+        //Second method, taking in a decimal and putting out an integer
+        public static decimal Method1(decimal decimal1)
+        {
+            //Math
+            decimal intermediate = decimal1 / 2;
+            //Convert to integer
+            int output = Convert.ToInt32(intermediate);
+            return output;
+        }
+
+        //Third method, taking in a string and putting out an integer
+        public static int Method1(string string1)
+        {
+            //Convert string to integer
+            int intermediate = Convert.ToInt32(string1);
+            //Math
+            int output = intermediate * 2;
+            return output;
+        }
+
+    }
+}
---
+++ MainMethodSubmissionAssn/MainMethodSubmissionAssn/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace MainMethodSubmissionAssn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Get a number, convert and store it
+            Console.WriteLine("Please input an integer");
+            int input = Convert.ToInt32(Console.ReadLine());
+            
+            //Instantiate our class, set the number field to the input number
+            Class1 instance1 = new Class1
+            {
+                number = input
+            };
+            //Call the first method with the input number as the argument
+            int output1 = Class1.Method1(instance1.number);
+            //Write the result
+            Console.WriteLine(output1);
+
+            //Same idea but convert the input number to decimal
+            Console.WriteLine("Please input a decimal number");
+            decimal inputDec = Convert.ToDecimal(Console.ReadLine());
+            Class1 instance2 = new Class1
+            {
+                decNumber = inputDec
+            };
+            //Call the second method on the decimal, output it
+            decimal output2 = Class1.Method1(instance2.decNumber);
+            Console.WriteLine(output2);
+
+            //This time let the third method convert the input string into integer
+            //Then do math and output the result
+            Console.WriteLine("Please input another integer");
+            string inputString = Console.ReadLine();
+            Class1 instance3 = new Class1
+            {
+                word = inputString
+            };
+            int output3 = Class1.Method1(instance3.word);
+            Console.WriteLine(output3);
+        }
+    }
+}
