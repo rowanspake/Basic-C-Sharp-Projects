47607aa5ac7f6a4837709fc9ec60d579b7118222
rowanspake <109189967+rowanspake@users.noreply.github.com>
2023-04-16 00:32:16 -0700

    Created MethodClassSubmissionAssn

---
+++ MethodClassSubmissionAssn/MethodClassSubmissionAssn/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodClassSubmissionAssn
+{
+    //Declare class
+    class Class1
+    {
+        //Declare class constructor
+        public Class1()
+        {
+
+        }
+
+        //Delare void method that takes two integer inputs
+        public static void Method1(int input1, int input2)
+        {
+            //Do math on first input
+            int result = input1 * 2;
+            //Write the other input to the console
+            Console.WriteLine(input2);
+        }
+
+
+    }
+}
---
+++ MethodClassSubmissionAssn/MethodClassSubmissionAssn/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MethodClassSubmissionAssn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate our class
+            Class1 class1 = new Class1();
+            //Call our method, give it two numbers
+            Class1.Method1(5, 4);
+            //Call our method again, this time specifying the parameters by name
+            Class1.Method1(input1: 3, input2: 6);
+
+        }
+    }
+}
