0bfdefaf9d9ce5e89c2307e737f4e68accd5502e
rowanspake <109189967+rowanspake@users.noreply.github.com>
2023-04-15 23:10:32 -0700

    Created MethodSubmissionAssn

---
+++ MethodSubmissionAssn/MethodSubmissionAssn/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodSubmissionAssn
+{
+    //Declare a class
+    class Class1
+    {
+        //Declare strings for constructor
+        public int input1;
+        public int input2;
+        //Declare constructor and initial string values
+        public Class1()
+        {
+            input1 = 0;
+            input2 = 5;
+        }
+        //Declare method with two integer inputs
+        public static int Method1(int input1, int input2)
+        {
+            //Do some math and return the result
+            int output1 = input1 * input2;
+            return output1;
+        }
+    }
+}
---
+++ MethodSubmissionAssn/MethodSubmissionAssn/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MethodSubmissionAssn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate our class
+            Class1 class1One = new Class1();
+
+            //Ask for first input, make it an integer, and store it in our class instance
+            Console.WriteLine("Please input a number");
+            int input1 = Convert.ToInt32(Console.ReadLine());
+            class1One.input1 = input1;
+
+            //Ask for another input number
+            Console.WriteLine("Please input another number. Or don't. I don't care.");
+            string input2string = Console.ReadLine();
+            
+            //If the input is null, we'll use the initial value we set in our class declaration
+            if (input2string == "")
+                Console.WriteLine("Okay, I'll pick my own second number\n" + class1One.input2);
+            //Otherwise, set the second value in our class instance to the second input
+            else
+                class1One.input2 = Convert.ToInt32(input2string);
+
+            //Call our class' method and give it our class instance's values
+            Console.WriteLine("Math!");
+            int output1 = Class1.Method1(class1One.input1, class1One.input2);
+            
+            //Display result
+            Console.WriteLine(output1);
+            Console.ReadLine();
+
+        }
+    }
+}
