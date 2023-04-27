f6c8d349420bc5295b9c734087a9a44c8c07f3b6
rowanspake <109189967+rowanspake@users.noreply.github.com>
2023-04-26 22:28:49 -0700

    Created StructAssn

---
+++ StructAssn/StructAssn/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace StructAssn
+{
+    class Program
+    {
+
+        //Create a struct with a decimal property
+        public struct Number
+        {
+            public decimal Amount;
+        }
+
+        static void Main(string[] args)
+        {
+            //Create object of the struct and assign a value to the decimal property
+            Number number1 = new Number
+            {
+                Amount = 0.5m
+            };
+
+            //Print it to the console
+            Console.WriteLine(number1.Amount);
+        }
+    }
+}
