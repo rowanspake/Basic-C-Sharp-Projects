e5765669dd5e85e9a66094e488ac5ef6818fdc40
rowanspake <109189967+rowanspake@users.noreply.github.com>
2023-04-27 23:00:57 -0700

    LambdaSubmissionAssn Created

---
+++ LambaSubmissionAssn/LambaSubmissionAssn/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LambaSubmissionAssn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create list of type Employee
+            List<Employee> employeeList = new List<Employee>();
+
+            //Add ten employees, two named Joe
+            employeeList.Add(new Employee( 0001, "Joe","Leen" ));
+            employeeList.Add(new Employee( 0002, "Bill", "Gronkowski" ));
+            employeeList.Add(new Employee( 0003, "Kim", "Bop" ));
+            employeeList.Add(new Employee( 0004, "Poppy", "McFlower" ));
+            employeeList.Add(new Employee( 0005, "Chum", "L\'Buqet" ));
+            employeeList.Add(new Employee( 0006, "Mari", "O\'World" ));
+            employeeList.Add(new Employee( 0007, "Phil", "Mahart" ));
+            employeeList.Add(new Employee( 0008, "Joe", "Naandthewail" ));
+            employeeList.Add(new Employee( 0009, "Hotcuppa", "Joe" ));
+            employeeList.Add(new Employee( 0010, "Tim", "Beurre" ));
+
+            //Create empty list for foreach loop
+            List<Employee> joeListForeach = new List<Employee>();
+
+            //Foreach loop that goes through our employee list and adds Joes to the empty list
+            foreach (Employee employee in employeeList)
+            {
+                if (employee.FirstName == "Joe")
+                    joeListForeach.Add(employee);
+            }
+
+            //Print the contents of the Joe list
+            foreach (Employee employee in joeListForeach)
+            {
+                Console.WriteLine("Employee number " + employee.Id + " has the first name \"Joe\"");
+            }
+
+            //Lambda expression that makes a new list and adds all Joes from the employee list
+            List<Employee> joeListLambda = employeeList.Where(x => x.FirstName == "Joe").ToList();
+
+            //Print the contents of the new Joe list
+            foreach (Employee employee in joeListLambda)
+            {
+                Console.WriteLine("\"Joe\" is the first name of employee number " + employee.Id);
+            }
+
+            //Lambda expression that makes a new list of employees with ID numbers over five
+            List<Employee> overFiveListLambda = employeeList.Where(x => x.Id > 5).ToList();
+            
+            //Print the results of the over five list
+            foreach (Employee employee in overFiveListLambda)
+            {
+                Console.WriteLine(employee.FirstName + " " + employee.LastName + " has an ID number over five.");
+            }
+        }
+    }
+}
--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -10,12 +10,14 @@ namespace TwentyOne
     { 
         static void Main(string[] args)
         {
+            ////Polymorphism
             //TwentyOneGame game = new TwentyOneGame();
             //game.Players = new List<string>() { "Jesse", "Bill", "Bob" };
             //game.ListPlayers();
 
             //Console.ReadLine();
 
+            ////Overloading operators
             //Game game = new TwentyOneGame();
             //game.Players = new List<Player>();
             //Player player = new Player();
@@ -23,14 +25,29 @@ namespace TwentyOne
             //game = game + player;
             //game = game - player;
 
-            Card card1 = new Card();
-            Card card2 = card1;
-            card1.Face = Face.Eight;
-            card2.Face = Face.King;
+            ////Structs
+            //Card card1 = new Card();
+            //Card card2 = card1;
+            //card1.Face = Face.Eight;
+            //card2.Face = Face.King;
 
-            Console.WriteLine(card1.Face);  
+            //Console.WriteLine(card1.Face);
 
-            //Deck deck = new Deck();
+            Deck deck = new Deck();
+
+            ////Lambdas
+            //int count = deck.Cards.Count(x => x.Face == Face.Ace);
+
+            //List<Card> newList = deck.Cards.Where(x => x.Face == Face.King).ToList();
+
+            List<int> numberList = new List<int>() { 1, 2, 3, 535, 342, 23 };
+
+            int sum = numberList.Where(x => x > 20).Sum();
+
+             Console.WriteLine(sum);
+                        
+
+
             //deck.Shuffle(3);
 
             //foreach (Card card in deck.Cards)
