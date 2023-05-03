f3382f4394d3def859c750c159dec671deca3696
rowanspake <109189967+rowanspake@users.noreply.github.com>
2023-05-02 21:54:54 -0700

    Created DateTimeSubmissionAssn

---
+++ DateTimeSubmissionAssn/DateTimeSubmissionAssn/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace DateTimeSubmissionAssn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Get the current date and time
+            DateTime timeNow = DateTime.Now;
+            //Print it
+            Console.WriteLine(timeNow);
+
+            //Ask for a number input
+            Console.WriteLine("Give me a number");
+            //Store it as an integer
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            //Declare a TimeSpan with the value of the user input for the hours value
+            TimeSpan userDate = new TimeSpan(0, userInput, 0, 0);
+            //Add those hours to the current date and time
+            DateTime laterDate = timeNow + userDate;
+            //Print the results
+            Console.WriteLine("In {0} hours it will be {1}.", userInput, laterDate);
+
+        }
+    }
+}
--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -12,7 +12,6 @@ namespace TwentyOne
         static void Main(string[] args)
         {
 
-
             Console.WriteLine("Welcome to the Grand Hotel and Casino.  Let's start by telling me your name.");
             string playerName = Console.ReadLine();
             Console.WriteLine("And how much money did you bring today?");
