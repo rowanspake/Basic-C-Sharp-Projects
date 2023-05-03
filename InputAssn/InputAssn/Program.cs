19945db27b2e09b113aeaf38e0c7791574cd3965
rowanspake <109189967+rowanspake@users.noreply.github.com>
2023-05-02 21:26:58 -0700

    Created InputAssn

---
+++ InputAssn/InputAssn/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace InputAssn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ask for a number
+            Console.WriteLine("Please input a number");
+            //Store input as a string
+            string input = Console.ReadLine();
+            //Use StreamWriter to access our log file and add the user input to the file
+            using (StreamWriter file = new StreamWriter(@"C:\Users\15036\OneDrive\Documents\Logs\inputlog.txt", true))
+            {
+                file.WriteLine(input);
+            }
+            //Read the contents of the file back to the user
+            Console.WriteLine(File.ReadAllText(@"C:\Users\15036\OneDrive\Documents\Logs\inputlog.txt"));
+
+        }
+    }
+}
--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace TwentyOne
 {
@@ -10,52 +11,30 @@ namespace TwentyOne
     { 
         static void Main(string[] args)
         {
-            ////Polymorphism
-            //TwentyOneGame game = new TwentyOneGame();
-            //game.Players = new List<string>() { "Jesse", "Bill", "Bob" };
-            //game.ListPlayers();
-
-            //Console.ReadLine();
-
-            ////Overloading operators
-            //Game game = new TwentyOneGame();
-            //game.Players = new List<Player>();
-            //Player player = new Player();
-            //player.Name = "Jesse";
-            //game = game + player;
-            //game = game - player;
-
-            ////Structs
-            //Card card1 = new Card();
-            //Card card2 = card1;
-            //card1.Face = Face.Eight;
-            //card2.Face = Face.King;
-
-            //Console.WriteLine(card1.Face);
-
-            Deck deck = new Deck();
-
-            ////Lambdas
-            //int count = deck.Cards.Count(x => x.Face == Face.Ace);
-
-            //List<Card> newList = deck.Cards.Where(x => x.Face == Face.King).ToList();
-
-            List<int> numberList = new List<int>() { 1, 2, 3, 535, 342, 23 };
-
-            int sum = numberList.Where(x => x > 20).Sum();
-
-             Console.WriteLine(sum);
-                        
 
 
-            //deck.Shuffle(3);
+            Console.WriteLine("Welcome to the Grand Hotel and Casino.  Let's start by telling me your name.");
+            string playerName = Console.ReadLine();
+            Console.WriteLine("And how much money did you bring today?");
+            int bank = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hello, {0}. Would you like to join a game of 21 right now?", playerName);
+            string answer = Console.ReadLine().ToLower();
+            if (answer == "yes" || answer == "yeah" || answer == "y" || answer == "ya")
+            {
+                Player player = new Player(playerName, bank);
+                Game game = new TwentyOneGame();
+                game += player;
+                player.isActivelyPlaying = true;
+                while (player.isActivelyPlaying && player.Balance > 0)
+                {
+                    game.Play();
+                }
+                game -= player;
+                Console.WriteLine("Thank you for playing!");
+            }
+            Console.WriteLine("Feel free to look around the casino. Bye for now.");
+            Console.Read();
 
-            //foreach (Card card in deck.Cards)
-            //{
-            //    Console.WriteLine(card.Face + " of " + card.Suit);
-            //}
-            //Console.WriteLine(deck.Cards.Count);
-            Console.ReadLine();
         }
     }
 }
